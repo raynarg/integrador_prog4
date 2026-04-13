e3d7dfcec9da3fb30b540e67a989b1b5eaa2676d
raynarg <santiagorayn24@gmail.com>
2026-04-13 13:15:47 -0300

    carpeta public

--- src/app.js
+++ src/app.js
@@ -1,22 +1,12 @@
 const express = require('express');
-const pool = require('./db');
 require('dotenv').config();
-
+const pool = require('./db'); // Mantenemos esto para que al arrancar te diga "Conexión exitosa"
 const app = express();
-app.use(express.json());
 
-// Ruta para probar que trae los cursos del integrador
-app.get('/api/cursos', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM public.cursos');
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Error al conectar con la DB' });
-  }
-});
+// Esta línea hace que todo lo que pongas en la carpeta "public" se vea en el navegador
+app.use(express.static('src/public'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor en http://localhost:${PORT}`);
+    console.log(`🚀 Servidor listo para maquetar en http://localhost:${PORT}`);
 });
