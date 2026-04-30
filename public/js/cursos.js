82ff1fa81b307d1e1a559d41279e81efbbab1d4a
raynarg <santiagorayn24@gmail.com>
2026-04-30 15:57:59 -0300

    ver acciones botones generados por JS

--- public/js/cursos.js
+++ public/js/cursos.js
@@ -17,17 +17,51 @@ document.addEventListener("DOMContentLoaded", async function () {
                 <td>${curso.id_curso}</td>
                 <td>${curso.nombre}</td>
                 <td>${curso.inscriptos_max}</td>
-                <td>${curso.id_curso_estado}</td>
                 <td>
-                    <!--Asi añado los botones con icono-->
-                    <button
-                        class="btn btn-sm btn-info"
-                        data-bs-toggle="modal"
-                        data-bs-target="#modalDetalle"
-                        title="Ver Detalle"
-                    >
-                        <i class="bi bi-eye"></i>
-                    </button>
+                    <span class="badge text-bg-success-subtle text-success border border-success-subtle">
+                    ${curso.id_curso_estado}
+                    </span>
+                </td>
+                <td class="text-end">
+                    <div class="btn-group btn-group-sm" role="group"
+                    <!--Añadir botones que abran los dialog. Importante: esto se hace por el data-bs-target y toggle-->
+                        <button
+                            class="btn btn-outline-success py-0 px-2"
+                            data-bs-toggle="modal"
+                            data-bs-target="#modalDiploma"
+                            data-id="curso.id_curso"
+                            title="Generar Diploma"
+                        >
+                            <i class="bi bi-award"></i>
+                        </button>
+                        <button
+                            class="btn btn-outline-primary py-0 px-2"
+                            data-bs-toggle="modal"
+                            data-bs-target="#modalDetalle"
+                            data-id="curso.id_curso"
+                            title="Ver Detalle"
+                        >
+                            <i class="bi bi-eye"></i>
+                        </button>
+                        <button
+                            class="btn btn-outline-warning py-0 px-2"
+                            data-bs-toggle=""
+                            data-bs-target=""
+                            data-id="curso.id_curso"
+                            title="Editar Curso"
+                        >
+                            <i class="bi bi-pencil"></i>
+                        </button>
+                        <button
+                            class="btn btn-outline-danger py-0 px-2"
+                            data-bs-toggle=""
+                            data-bs-target=""
+                            data-id="curso.id_curso"
+                            title="Eliminar Curso"
+                        >
+                            <i class="bi bi-trash"></i>
+                        </button>
+                    </div>
                 </td>
             `;
             tabla.appendChild(fila);
